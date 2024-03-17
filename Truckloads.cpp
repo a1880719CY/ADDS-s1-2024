860d7be96b1e4979405a461af8ecda64de2e7467
a1880719CY <a1880719@adelaide.edu.au>
2024-03-17 21:09:45 +1030

    q1v0.2

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -4,16 +4,24 @@
 int Truckloads::numTrucks(int numCrates, int loadSize){
     static int numTrckNeed = 1;
     cout << numTrckNeed << endl;
-    if (numCrates <= loadSize){
+    cout << "Current crate:"<< numCrates << endl;
+    int subCrateNum = numCrates/2;
+    if (numCrates%2 == 1){
+        if (subCrateNum <= loadSize){
+            numTrckNeed++;
+            cout<<"Should add 1"<<endl;
+        }
+        subCrateNum++;
+    }
+    if (subCrateNum <= loadSize){
 
         return numTrckNeed;
 
     } else {
-
-        int subCrateNum = numCrates/2;
+        
+        //final part, just don't touch
         numTrckNeed = numTrckNeed*2;
         return numTrucks(subCrateNum, loadSize);
-
     }
 
 }
