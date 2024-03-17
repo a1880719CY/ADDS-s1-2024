e866092bde61819a82cfc91cfd7ffa1e443f045e
a1880719CY <a1880719@adelaide.edu.au>
2024-03-17 20:36:22 +1030

    q1V0.1

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
+    static int numTrckNeed = 1;
+    cout << numTrckNeed << endl;
+    if (numCrates <= loadSize){
 
+        return numTrckNeed;
+
+    } else {
+
+        int subCrateNum = numCrates/2;
+        numTrckNeed = numTrckNeed*2;
+        return numTrucks(subCrateNum, loadSize);
+
+    }
 
 }
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "Truckloads.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    Truckloads truck1;
+    cout << truck1.numTrucks(14,3);
+    
+}
